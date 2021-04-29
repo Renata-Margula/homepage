88f899fe18468cde213fd2dea97b5e1f5ffa3235
Renata Margula <reniutaaa@gmail.com>
2021-04-29 20:32:38 +0200

    change to the arrow function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
-console.log("HELLO WORLD");
+{
 
-let idyllPhoto = document.querySelector(".weatherWindowPhoto");
-let weatherWindowPhoto = document.querySelector(".idyllPhoto");
-let myPhotos__changeButton = document.querySelector(".myPhotos__changeButton");
-let aboutMe__button = document.querySelector(".aboutMe__button");
-let aboutMe__pictureOfMe = document.querySelector(".aboutMe__pictureOfMe");
+const welcome = () => {
+  console.log("HELLO WORLD");
+}
+welcome();
 
-myPhotos__changeButton.addEventListener("click", () => {
-  idyllPhoto.classList.toggle("myPhotos__hide");
-  weatherWindowPhoto.classList.toggle("myPhotos__hide");
-});
-aboutMe__button.addEventListener("click", () => {
+const onHideShowPhotoClick = () => {
   aboutMe__pictureOfMe.classList.toggle("aboutMe__pictureOfMe--hidden");
   aboutMe__button.innerHTML =
     aboutMe__button.innerHTML === "Schowaj mnie" ? "Przywróć" : "Schowaj mnie";
-});
+}
+const aboutMe__button = document.querySelector(".aboutMe__button");
+const aboutMe__pictureOfMe = document.querySelector(".aboutMe__pictureOfMe");
+aboutMe__button.addEventListener("click", onHideShowPhotoClick);
+
+}
+
+{
+  const onChangePhotoClick = () => {
+    idyllPhoto.classList.toggle("myPhotos__hide");
+    weatherWindowPhoto.classList.toggle("myPhotos__hide");
+  }
+const idyllPhoto = document.querySelector(".weatherWindowPhoto");
+const weatherWindowPhoto = document.querySelector(".idyllPhoto");
+const myPhotos__changeButton = document.querySelector(".myPhotos__changeButton");
+
+myPhotos__changeButton.addEventListener("click", onChangePhotoClick);
+
+}
+
