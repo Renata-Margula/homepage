edfc03987a9018402b7392f819571db5d3ab4e5f
Renata Margula <reniutaaa@gmail.com>
2021-05-09 20:20:17 +0200

    changed modifier -myPhotos

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 {
 
-const onHideShowPhotoClick = () => {
-  aboutMe__pictureOfMe.classList.toggle("aboutMe__pictureOfMe--hidden");
-  aboutMe__button.innerHTML =
-    aboutMe__button.innerHTML === "Schowaj mnie" ? "Przywróć" : "Schowaj mnie";
-}
-const aboutMe__button = document.querySelector(".aboutMe__button");
-const aboutMe__pictureOfMe = document.querySelector(".aboutMe__pictureOfMe");
-aboutMe__button.addEventListener("click", onHideShowPhotoClick);
+  const onHideShowPhotoClick = () => {
+    aboutMe__pictureOfMe.classList.toggle("aboutMe__pictureOfMe--hidden");
+    aboutMe__button.innerHTML =
+      aboutMe__button.innerHTML === "Schowaj mnie" ? "Przywróć" : "Schowaj mnie";
+  }
+  const aboutMe__button = document.querySelector(".aboutMe__button");
+  const aboutMe__pictureOfMe = document.querySelector(".aboutMe__pictureOfMe");
+  aboutMe__button.addEventListener("click", onHideShowPhotoClick);
 
 }
 
 {
   const onChangePhotoClick = () => {
-    idyllPhoto.classList.toggle("myPhotos__hide");
-    weatherWindowPhoto.classList.toggle("myPhotos__hide");
+    idyllPhoto.classList.toggle("myPhotos--hide");
+    weatherWindowPhoto.classList.toggle("myPhotos--hide");
   }
-const idyllPhoto = document.querySelector(".weatherWindowPhoto");
-const weatherWindowPhoto = document.querySelector(".idyllPhoto");
-const myPhotos__changeButton = document.querySelector(".myPhotos__changeButton");
+  const idyllPhoto = document.querySelector(".weatherWindowPhoto");
+  const weatherWindowPhoto = document.querySelector(".idyllPhoto");
+  const myPhotos__changeButton = document.querySelector(".myPhotos__changeButton");
 
-myPhotos__changeButton.addEventListener("click", onChangePhotoClick);
+  myPhotos__changeButton.addEventListener("click", onChangePhotoClick);
 
 }
 
